f5e2c6f78ae8989eae036ced39d0469db69fc114
possible <sumitkumar66416@gmail.com>
2024-12-17 17:46:42 +0530

    minor  fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
 	useEffect(() => {
 		fetchShows('trending');
-	}, [fetchShows])
+	}, [])
 
 	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchQuery(e.target.value)
